16b7d7c6e216df5e77f48c9474e554d48b731a53
Inacio Filho <inaciofilho.lima@gmail.com>
2024-05-09 21:01:52 -0300

    Initial Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import logo from "@/assets/Employ-logo-cor-fundo-branco.png"
+import Image from "next/image"
+import { ButtonLinkMenu } from "./ButtonLinkMenu"
+export function Header(){
+    return (
+        <header >
+            
+        </header>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        'primary-green': '#0FFC76',
+        'contrast-green': '#46A31D',
+        'second-green': '#1EA358',
+        'backgrond-color': '#F8F8F8'
+      }
     },
   },
   plugins: [],
